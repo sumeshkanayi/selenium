09bfc0b8fb52e51125caa24ee98c6b5ade9bb969
sumeshkanayi <sumeshkanayi@gmail.com>
2016-06-05 14:49:37 +0530

    Update SeleniumStandaloneServerTest.java

--- selenium/src/test/java/ru/parsentev/SeleniumStandaloneServerTest.java
+++ selenium/src/test/java/ru/parsentev/SeleniumStandaloneServerTest.java
@@ -21,10 +21,12 @@ import static org.junit.Assert.assertThat;
  * @since 19.11.2015
  */
 public class SeleniumStandaloneServerTest {
+	
+	System.setProperty("webdriver.chrome.driver", "c://code//chromedriver.exe"
 
 	@Test
 	public void executeFirefoxDriver() throws MalformedURLException {
-		DesiredCapabilities capability = DesiredCapabilities.firefox();
+		DesiredCapabilities capability = DesiredCapabilities.chrome();
 		WebDriver driver = new RemoteWebDriver(
 				new URL("http://10.0.0.47:4444/wd/hub"), capability
 		);
